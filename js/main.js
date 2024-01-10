91d7fce647ffd2e46636dc26cc969b6352a502e9
Gabriel Cuzzio <gabriel8724@gmail.com>
2024-01-10 14:45:07 -0300

    Orden

--- js/main.js
+++ js/main.js
@@ -66,48 +66,48 @@ openFormBtn.addEventListener('click', toggleForm);
 
 /*--------------------Marcas con las que se trabaja---------------------------*/
 
-document.addEventListener('DOMContentLoaded', function () {
-  const banner = document.querySelector('.banner');
-  const prevBtn = document.querySelector('.prev-btn');
-  const nextBtn = document.querySelector('.next-btn');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const banner = document.querySelector('.banner');
+//   const prevBtn = document.querySelector('.prev-btn');
+//   const nextBtn = document.querySelector('.next-btn');
 
-  let currentIndex = 0;
-  const totalLogos = 16; // Cambia esto al total de logotipos que tengas
+//   let currentIndex = 0;
+//   const totalLogos = 16; // Cambia esto al total de logotipos que tengas
 
-  prevBtn.addEventListener('click', function () {
-    currentIndex = (currentIndex - 1 + totalLogos) % totalLogos;
-    updateBanner();
-  });
+//   prevBtn.addEventListener('click', function () {
+//     currentIndex = (currentIndex - 1 + totalLogos) % totalLogos;
+//     updateBanner();
+//   });
 
-  nextBtn.addEventListener('click', function () {
-    currentIndex = (currentIndex + 1) % totalLogos;
-    updateBanner();
-  });
+//   nextBtn.addEventListener('click', function () {
+//     currentIndex = (currentIndex + 1) % totalLogos;
+//     updateBanner();
+//   });
 
-  function updateBanner() {
-    const translateValue = -currentIndex * (5 + 1.5); // 4cm de alto + 2cm de margen
-    banner.style.transition = 'transform 0.5s ease-in-out';
-    banner.style.transform = `translateX(${translateValue}cm)`;
-  }
+//   function updateBanner() {
+//     const translateValue = -currentIndex * (5 + 1.5); // 4cm de alto + 2cm de margen
+//     banner.style.transition = 'transform 0.5s ease-in-out';
+//     banner.style.transform = `translateX(${translateValue}cm)`;
+//   }
 
-  // Agregamos un listener para el evento de transición
-  banner.addEventListener('transitionend', function () {
-    // Al finalizar la transición, comprobamos si estamos en el último logo y ajustamos el índice
-    if (currentIndex === totalLogos - 1) {
-      banner.style.transition = 'none'; // Eliminamos la transición para que el cambio sea instantáneo
-      currentIndex = 0;
-      const translateValue = -currentIndex * (5 + 1.5); 
-      banner.style.transform = `translateX(${translateValue}cm)`;
-      // Forzamos un repaint (repintado) para que la transición sin transición surta efecto
-      banner.offsetHeight; // eslint-disable-line no-unused-expressions
-      banner.style.transition = 'transform 0.5s ease-in-out';
-    }
-  });
+//   // Agregamos un listener para el evento de transición
+//   banner.addEventListener('transitionend', function () {
+//     // Al finalizar la transición, comprobamos si estamos en el último logo y ajustamos el índice
+//     if (currentIndex === totalLogos - 1) {
+//       banner.style.transition = 'none'; // Eliminamos la transición para que el cambio sea instantáneo
+//       currentIndex = 0;
+//       const translateValue = -currentIndex * (5 + 1.5); 
+//       banner.style.transform = `translateX(${translateValue}cm)`;
+//       // Forzamos un repaint (repintado) para que la transición sin transición surta efecto
+//       banner.offsetHeight; // eslint-disable-line no-unused-expressions
+//       banner.style.transition = 'transform 0.5s ease-in-out';
+//     }
+//   });
 
-  // Iniciamos el carrusel automáticamente
-  setInterval(function () {
-    currentIndex = (currentIndex + 1) % totalLogos;
-    updateBanner();
-  }, 2000); // Cambia 3000 a la duración deseada en milisegundos
-});
+//   // Iniciamos el carrusel automáticamente
+//   setInterval(function () {
+//     currentIndex = (currentIndex + 1) % totalLogos;
+//     updateBanner();
+//   }, 2000); // Cambia 3000 a la duración deseada en milisegundos
+// });
 
